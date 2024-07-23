8983e185fa0606cdd323aa88aa3097b5f636664e
laihaoqiang <laihaoqiang34@gmail.com>
2024-07-24 00:48:07 +1000

    navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,43 @@
-import Mine from "../assets/mine.png";
-
 const Navbar = () => {
   return (
     <div className="flex justify-around w-full fixed bottom-2 py-3 rounded-t-3xl border-t-2 border-blue-400">
       <a href="/shop">
         <div className="flex flex-col items-center">
-          <img src={Mine} alt="" width={30} />
+          <div className="scale-150">
+            <i className="bi bi-cart-fill"></i>
+          </div>
           Shop
         </div>
       </a>
       <a href="/earncash">
         <div className="flex flex-col items-center">
-          <img src={Mine} alt="" width={30} />
-          Social Task
+          <div className="scale-150">
+            <i class="bi bi-cash"></i>
+          </div>
+          Tasks
         </div>
       </a>
       <a href="/home">
         <div className="flex flex-col items-center">
-          <img src={Mine} alt="" width={30} />
+          <div className="scale-150">
+            <i className="bi bi-house-door-fill"></i>
+          </div>
           Home
         </div>
       </a>
       <a href="/friends">
         <div className="flex flex-col items-center">
-          <img src={Mine} alt="" width={30} />
+          <div className="scale-150">
+            <i className="bi bi-people-fill"></i>
+          </div>
           Friends
         </div>
       </a>
       <a href="/wallet">
         <div className="flex flex-col items-center">
-          <img src={Mine} alt="" width={30} />
+          <div className="scale-150">
+            <i className="bi bi-wallet-fill"></i>
+          </div>
           Wallet
         </div>
       </a>
