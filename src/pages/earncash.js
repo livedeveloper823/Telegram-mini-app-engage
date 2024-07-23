c5122bdf7d06f080417dfea193396a7c95c698e2
laihaoqiang <laihaoqiang34@gmail.com>
2024-07-23 17:46:50 +1000

    updated

--- src/pages/earncash.js
+++ src/pages/earncash.js
@@ -6,15 +6,15 @@ const Mine = () => {
       <div className="h-[90vh] w-[100vw]">
         <div className="w-full text-white text-center">
           <div className="flex justify-around mt-3">
-            <div className="bg-blue-600 rounded-2xl px-10 py-5 hover:translate-y-1 duration-150">
+            <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
               <p>Earn per Tap</p>
               <p>+8</p>
             </div>
-            <div className="bg-blue-600 rounded-2xl px-10 py-5 hover:translate-y-1 duration-150">
+            <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
               <p>Earn per Tap</p>
               <p>1M</p>
             </div>
-            <div className="bg-blue-600 rounded-2xl px-10 py-5 hover:translate-y-1 duration-150">
+            <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
               <p>Earn per Tap</p>
               <p>+612.5K</p>
             </div>
