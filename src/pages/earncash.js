3a21556fa72aec5a5f0435aa75174df352d78e9f
laihaoqiang <laihaoqiang34@gmail.com>
2024-07-27 01:37:39 +1000

    chart added

--- src/pages/earncash.js
+++ src/pages/earncash.js
@@ -10,26 +10,29 @@ const EarnCash = () => {
         </div>
         <div className="w-full text-white text-center">
           <div className="flex justify-around mt-2">
-            <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
-              <p>Discord</p>
-            </div>
-            <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
-              <p>Telegram</p>
-            </div>
-            <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
-              <p>Whatsapp</p>
-            </div>
-            <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
-              <p>Skype</p>
-            </div>
+            <a href="https://discord.com" target="blank">
+              <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
+                Discord
+              </div>
+            </a>
+            <a href="https://web.telegram.org" target="blank">
+              <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
+                Telegram
+              </div>
+            </a>
+            <a href="https://web.whatsapp.com" target="blank">
+              <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
+                Whatsapp
+              </div>
+            </a>
+            <a href="https://web.skype.com" target="blank">
+              <div className="bg-blue-600 rounded-xl px-5 py-3 hover:translate-y-1 duration-150">
+                Skype
+              </div>
+            </a>
           </div>
         </div>
         <div className="flex flex-col gap-3 mt-5 items-center">
-          <p>SOCIAL MEADIA TASK IN LIST VIEW & ORDER ID</p>
-          <p>SOCIAL MEADIA TASK IN LIST VIEW & ORDER ID</p>
-          <p>SOCIAL MEADIA TASK IN LIST VIEW & ORDER ID</p>
-          <p>SOCIAL MEADIA TASK IN LIST VIEW & ORDER ID</p>
-          <p>SOCIAL MEADIA TASK IN LIST VIEW & ORDER ID</p>
           <p>SOCIAL MEADIA TASK IN LIST VIEW & ORDER ID</p>
         </div>
       </div>
