6808be5bfdb03406d10bfd27e8bb323534512a10
DavidLamos <davidlamos4453@gmail.com>
2024-08-25 06:23:53 +0900

    fix:complete homepage & taskpage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="fixed left-0 bottom-0 w-full">
+    <div className="fixed left-0 bottom-0 w-full max-w-3xl">
       <div className="flex justify-around w-full box-border  py-3 rounded-t-2xl border-t-2 border-blue-400 bg-blue-500">
         <a href="/shop">
           <div className="flex flex-col items-center">
